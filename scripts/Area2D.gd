b46831ae4ef1b1f8a83aa5f002362abf74b630be
Daya Adianto <dayaadianto@cs.ui.ac.id>
2024-04-04 07:44:13 +0700

    Update paths

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -4,4 +4,4 @@ export (String) var sceneName = "Level 1"
 
 func _on_Area_Trigger_body_entered(body):
     if body.get_name() == "Player":
-        get_tree().change_scene(str("res://Scenes/" + sceneName + ".tscn"))
+        get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
